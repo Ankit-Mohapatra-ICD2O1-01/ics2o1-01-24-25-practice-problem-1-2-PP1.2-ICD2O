946433e4d0a2a986e3211b7da5345134fbaf8ce0
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-09-17 00:49:41 +0000

    trying to hand in my work

--- PP1_2.py
+++ PP1_2.py
@@ -3,18 +3,42 @@
 #Write Assignment code here
 
 def q1():
+    name = "Alex"
 
 def q2():
+    name1 = 1
+    table = 2
+    french = 3
+
+print( french + name1 + table)
+
 
 def q3():
+    variableThree = 3
+    variableFour = 4
+print(variableThree // variableFour)
 
 def q4():
+    float1 = 6.3
+    float2 = 3.0
+print(float1 / float2)
 
 def q5():
+    num = "hello world"
+print(num)
+    
+num = "goodbye world"
+print(num)
 
 def q6():
 
-def q7():
+    tables = True
+    print(tables)
+
+def q7(): 
+    number0ne = 7
+    numberTwo = 2
+print(number0ne / numberTwo)
 
 q1()
 q2()
