cb2bf9fb45ac8fb8331f0bac1f0cc52280f72b09
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-09-17 13:34:49 +0000

    hi

--- PP1_2.py
+++ PP1_2.py
@@ -9,23 +9,22 @@ def q2():
     name1 = 1
     table = 2
     french = 3
-
-print( french + name1 + table)
+    print( french + name1 + table)
 
 
 def q3():
     variableThree = 3
     variableFour = 4
-print(variableThree // variableFour)
+    print(variableThree // variableFour)
 
 def q4():
     float1 = 6.3
     float2 = 3.0
-print(float1 / float2)
+    print(float1 / float2)
 
 def q5():
     num = "hello world"
-print(num)
+    print(num)
     
 num = "goodbye world"
 print(num)
@@ -38,7 +37,7 @@ def q6():
 def q7(): 
     number0ne = 7
     numberTwo = 2
-print(number0ne / numberTwo)
+    print(number0ne / numberTwo)
 
 q1()
 q2()
